75a0d4aad7123c85d100e05887ad101d613b8fa9
jesusrocca <jesusroccaing@gmail.com>
2022-04-07 12:24:10 -0500

    one

---
+++ SUMA.py
@@ -0,0 +1,6 @@
+#SUMA DE NUMEROS
+print("SUMA DE ENTEROS")
+n1=int(input("Digite el primer  numero: "))
+n2=int(input("Digite el segiundo numero: "))
+RPTA=n1+n2
+print(f"La suma de {n1} + {n2} es {RPTA}")
